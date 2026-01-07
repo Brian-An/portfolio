83153650e33b5c8f4ed8c48c14a4ce8ef192abe5
Brian An <b5an@uwaterloo.ca>
2026-01-07 15:29:42 -0500

    project constants

---
+++ constants/projects.ts
@@ -0,0 +1,70 @@
+export const PROJECTS = [
+  {
+    title: "AdBrain",
+    description:
+      "An AI-powered ad generator that turns short product or event descriptions into high-quality promotional videos in seconds.",
+    link: "https://github.com/brian-fu/adbrain",
+    techStack: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "FastAPI",
+      "PostgreSQL",
+      "AWS S3",
+      "Google Veo 3",
+      "FFmpeg",
+    ],
+  },
+  {
+    title: "Jane Street Estimathon",
+    description:
+      "A real-time competition platform built to run team-based estimation and trivia challenges with live scoring and dashboard sponsored by Jane Street.",
+    link: "https://estimathon.vercel.app/",
+    techStack: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "PostgreSQL",
+      "Prisma",
+      "Redux",
+    ],
+  },
+  {
+    title: "DropLink",
+    description:
+      "A file sharing app that lets users upload files and instantly generate shareable download links.",
+    link: "https://droplink-28ro.onrender.com/",
+    techStack: ["Node.js", "Express", "MongoDB", "React"],
+  },
+  {
+    title: "Refyne",
+    description:
+      "An AI-powered behavioral interview prep app that uses real-time audio-visual feedback and analytics to help users improve their interview performance.",
+    link: "https://github.com/UOFTHACKSLEET/uofthacks-updated",
+    techStack: [
+      "React",
+      "Vite",
+      "TypeScript",
+      "Tailwind CSS",
+      "Flask",
+      "Python",
+      "OpenAI API",
+    ],
+  },
+  {
+    title: "C++ Chess Engine (CS 246: Object Oriented Programming)",
+    description:
+      "Designed a modular C++20 chess engine using factory and observer patterns, featuring a configurable decision-tree–based AI, low coupling architecture, and extensible gameplay features (undo, opening detection, move prediction).",
+    link: "https://student.cs.uwaterloo.ca/~cs246/W26/index.shtml",
+    techStack: ["C++20", "X11", "Makefile"],
+  },
+  {
+    title: "RevealAI (In Progress)",
+    description:
+      "A chrome extension that identifies AI-generated images by checking c2pa metadata and a lightweight computer vision model to look for invisible watermarks.",
+    link: "https://github.com/Brian-An/revealai",
+    techStack: ["React", "TypeScript", "Vite"],
+  },
+];
