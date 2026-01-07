0d8e82481acdaad2eecb0183ad159f3ae13f71df
Brian An <b5an@uwaterloo.ca>
2026-01-07 15:30:21 -0500

    project page

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import ProjectCard from "@/components/ProjectCard";
+import { PROJECTS } from "@/constants/projects";
+
 export default function Projects() {
   return (
-    <div className="max-w-3xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">HI</h1>
+    <div className="max-w-xl flex flex-col gap-4 mt-2 mb-2">
+      {PROJECTS.map((project, index) => (
+        <ProjectCard
+          key={index}
+          title={project.title}
+          description={project.description}
+          link={project.link}
+          techStack={project.techStack}
+        />
+      ))}
     </div>
   );
 }
