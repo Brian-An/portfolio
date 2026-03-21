0796e039e6fa5df21e2d28604d2a3682608f47d2
Brian An <b5an@uwaterloo.ca>
2026-03-21 01:18:21 -0400

    x social and use react icon

--- constants/socials.ts
+++ constants/socials.ts
@@ -1,7 +1,15 @@
-import { Github, Linkedin, Mail } from "lucide-react";
+import { IconType } from "react-icons";
+import { FiMail, FiGithub, FiLinkedin } from "react-icons/fi";
+import { FaXTwitter } from "react-icons/fa6";
 
-export const socialLinks = [
-  { href: "mailto:brian.an1@uwaterloo.ca", icon: Mail },
-  { href: "https://www.linkedin.com/in/brian-an06/", icon: Linkedin },
-  { href: "https://github.com/Brian-An", icon: Github },
+export interface SocialLink {
+  href: string;
+  icon: IconType;
+}
+
+export const socialLinks: SocialLink[] = [
+  { href: "mailto:brian.an1@uwaterloo.ca", icon: FiMail },
+  { href: "https://www.linkedin.com/in/brian-an06/", icon: FiLinkedin },
+  { href: "https://github.com/Brian-An", icon: FiGithub },
+  { href: "https://www.x.com/imbrianan", icon: FaXTwitter },
 ];
